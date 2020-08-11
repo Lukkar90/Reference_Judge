6affd8d55f8bdda0eea49f0897fe13d00d866431
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-08-11 15:36:47 +0200

    fixed name position

--- add_text.py
+++ add_text.py
@@ -6,28 +6,30 @@ import cv2
 import numpy as np
 
 
-def add_text(img, text_description="None", font=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.20, fontColor=(0, 0, 0), lineType=1):
+def add_text(img, text_description="None", font=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.20, fontColor=(0, 0, 0), thickness=0):
 
     # check if canvas is to small to add text
     if image_is_bigger_than(100, img):
 
         # init value
-        img_w, img_h = img.shape[:2]
+        img_h, img_w = img.shape[:2]
 
         # add 10% white padding
         percent = 10
         img = add_white_space(img, pad_bot=percent)
 
         # calculate proportional text values
-        bottomLeftCornerOfText, fontScale, lineType = calculate_text_values(
+        bottomLeftCornerOfText, fontScale, thickness = calculate_text_values(
             img_w,
-            lineType,
+            img_h,
             text_description,
             font,
-            img_h,
+            thickness,
             percent,
             fontScale
         )
+
+        print(img_w, img_h, bottomLeftCornerOfText)
 
         # add text to image
         cv2.putText(
@@ -37,7 +39,7 @@ def add_text(img, text_description="None", font=cv2.FONT_HERSHEY_SIMPLEX, fontSc
             font,
             fontScale,
             fontColor,
-            lineType
+            thickness,
         )
 
     return img
@@ -77,23 +79,25 @@ def calculate_pads_values(img, pad_top=0, pad_bot=0, pad_left=0, pad_right=0):
     return (pad_top, pad_bot, pad_left, pad_right)
 
 
-def calculate_text_values(img_w, lineType, text_description, font, img_h, percent, fontScale):
+def calculate_text_values(img_w, img_h, text_description, font, thickness, percent, fontScale):
 
     # calculate ratio to resize all values
     resize = get_scale_value(img_w)
 
     # rescale values
-    fontScale, lineType = rescale_values(resize, fontScale, lineType)
+    fontScale, thickness = rescale_values(resize, fontScale, thickness)
 
     # get values of co-ordinates of text
     coord_w, coord_h = get_middle_coords_of_pad(
-        text_description, font, fontScale, lineType, img_h, percent, img_w)
+        text_description, font, fontScale, thickness, img_h, percent, img_w)
+
+    print(img_w, img_h)
 
     # pass co-ordinates
     middle_of_padding = (
         coord_w, coord_h)
 
-    return middle_of_padding, fontScale, lineType
+    return middle_of_padding, fontScale, thickness
 
 
 def get_scale_value(img_w):
@@ -103,21 +107,21 @@ def get_scale_value(img_w):
     return resize
 
 
-def rescale_values(resize, fontScale, lineType):
+def rescale_values(resize, fontScale, thickness):
 
     fontScale *= resize
-    lineType = math.ceil(lineType * resize)
-    return fontScale, lineType
+    thickness = math.ceil(thickness * resize)
+    return fontScale, thickness
 
 
-def get_middle_coords_of_pad(text_description, font, fontScale, lineType, img_h, percent, img_w):
+def get_middle_coords_of_pad(text_description, font, fontScale, thickness, img_h, percent, img_w):
     text_width, text_height = cv2.getTextSize(
-        text_description, font, fontScale, lineType)[0]
+        text_description, font, fontScale, thickness)[0]
 
     pad_value = give_pad_value(img_h, percent)
 
     coord_w = int(img_w/2) - int(text_width/2)
-    coord_h = img_h + int(pad_value/1.5)
+    coord_h = img_h + int(pad_value/2)
 
     return coord_w, coord_h
 
