0d28c5c2ed9f3156c0910b6824db7f0348975ae4
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-08-14 19:25:11 +0200

    reorder __main__

--- Reference_Judge/__main__.py
+++ Reference_Judge/__main__.py
@@ -36,56 +36,88 @@ from config.config import ARGV
 from add_text_to_image.add_text_to_image import add_text_to_image, is_bigger_than
 
 
-def resize_all(images, width):
-    """Change all image size keeping ratio"""
+def main():
+    """Parsing sys.argv to pass them into right functions"""
 
-    for image in images:
-        if not image == "Original_name":  # This is the only value in dict which is not a image
-            resize = resize_with_with_aspect_ratio(images[image], width)
-            images[image] = resize
+    check_argv_correctness(sys.argv)
+    program_help(sys.argv)
 
-    return images
+    # Init variables
+    original_ref_path = sys.argv[1]
+    app_ref_path = sys.argv[2]
+    mode = sys.argv[3]
+
+    # [-1] To avoid checking 3 places at one, this argument is always last
+    by_ratio = bool(sys.argv[-1] in ARGV["search by ratio"])
+
+    similar_list = create_similar_images_list(
+        original_ref_path, app_ref_path, by_ratio)
+
+    width = 360  # Default
+
+    if mode in ARGV["save"]:
+
+        mode_save(width, similar_list, by_ratio)
+
+    elif mode in ARGV["show"]:
+
+        # Optional args
+        mode_show(width, similar_list, by_ratio)
+    else:
+        raise ValueError("Error: Invalid mode value\n"
+                         f" {mode}")
+
+    sys.exit(0)
 
 
-def format_path(temp_dir, temp_name, index, temp_ext):
-    """example_dir_path/file-0000%.ext"""
+def mode_show(width, similar_list, by_ratio):
+    """show matched images"""
 
-    return f"{temp_dir}/{temp_name}-{str(index).zfill(5)}.{temp_ext}"
+    # Optional args
+    if len(sys.argv) >= 6:
+        width = parse_optional_argvs(sys.argv, 6, width)
+
+    # Process all images, show user each sequence one by one
+    for similar_pair in similar_list:
+
+        if not similar_pair is None:
+
+            images = compute_image_differences(similar_pair, by_ratio)
+
+            show_images(images, width)
+
+            print('NOTE: Press the "0" key, to close opened windows')
+            cv2.waitKey(0)
 
 
-def next_path(path_pattern):  # https://stackoverflow.com/a/47087513/12490791
-    """
-    Finds the next free path in an sequentially named list of files
+def mode_save(width, similar_list, by_ratio):
+    """save matched images in chosen directory"""
 
-    e.g. path_pattern = 'file-%s.txt':
+    if len(sys.argv) >= 5:
+        output_path = sys.argv[4]
+    else:
+        output_path = None
 
-    file-00001.txt
-    file-00002.txt
-    file-00003.txt
+    # Optional args
+    if len(sys.argv) >= 6:
+        width = parse_optional_argvs(sys.argv, 7, width)
 
-    Runs in log(n) time where n is the number of existing files in sequence
-    """
-    temp_dir = os.path.dirname(path_pattern)
-    temp_full_name = os.path.basename(path_pattern)
-    # https://stackoverflow.com/a/6670331/12490791
-    temp_name, temp_ext = temp_full_name.split('.', 1)
+    # Process all images, save each sequence in chosen director
+    for similar_pair in similar_list:
 
-    i = 1
+        if not similar_pair is None:
 
-    # First do an exponential search
-    while os.path.exists(format_path(temp_dir, temp_name, i, temp_ext)):
-        i = i * 2
+            images = compute_image_differences(similar_pair, by_ratio)
 
-    # Result lies somewhere in the interval (i/2..i]
-    # We call this interval (first..last] and narrow it down until first + 1 = last
-    first, last = (i // 2, i)
-    while first + 1 < last:
-        mid = (first + last) // 2  # interval midpoint
-        first, last = (mid, last) if os.path.exists(format_path(
-            temp_dir, temp_name, mid, temp_ext)) else (first, mid)
+            save_images_as_one(images, output_path, width)
 
-    # .replace("\\", "/") to make path string more consistent
-    return format_path(temp_dir, temp_name, last, temp_ext).replace("\\", "/")
+
+def parse_optional_argvs(argv_, cap_len_argv, DEFAULT_width):
+    """check if width, search_by_ratio are correct. If both are it returns width"""
+
+    check_correctness_optional_argvs(argv_, cap_len_argv)
+
+    return retrieve_width(argv_, cap_len_argv, DEFAULT_width)
 
 
 def show_images(images, width):
@@ -110,7 +142,7 @@ def show_images(images, width):
 
 
 def save_images_as_one(images, output_path, width):
-    """save app and ref images with images showing differences in one file"""
+    """save app and ref images with images showing differences in one image"""
 
     # Resize to default value or custom
     images = resize_all(images, width)
@@ -223,6 +255,58 @@ def check_correctness_optional_argvs(argv_, cap_len_argv):
                              f" {argv_[n -1]}")
 
 
+def resize_all(images, width):
+    """Change all image size keeping ratio"""
+
+    for image in images:
+        if not image == "Original_name":  # This is the only value in dict which is not a image
+            resize = resize_with_with_aspect_ratio(images[image], width)
+            images[image] = resize
+
+    return images
+
+
+def next_path(path_pattern):  # https://stackoverflow.com/a/47087513/12490791
+    """
+    Finds the next free path in an sequentially named list of files
+
+    e.g. path_pattern = 'file-%s.txt':
+
+    file-00001.txt
+    file-00002.txt
+    file-00003.txt
+
+    Runs in log(n) time where n is the number of existing files in sequence
+    """
+    temp_dir = os.path.dirname(path_pattern)
+    temp_full_name = os.path.basename(path_pattern)
+    # https://stackoverflow.com/a/6670331/12490791
+    temp_name, temp_ext = temp_full_name.split('.', 1)
+
+    i = 1
+
+    # First do an exponential search
+    while os.path.exists(format_path(temp_dir, temp_name, i, temp_ext)):
+        i = i * 2
+
+    # Result lies somewhere in the interval (i/2..i]
+    # We call this interval (first..last] and narrow it down until first + 1 = last
+    first, last = (i // 2, i)
+    while first + 1 < last:
+        mid = (first + last) // 2  # interval midpoint
+        first, last = (mid, last) if os.path.exists(format_path(
+            temp_dir, temp_name, mid, temp_ext)) else (first, mid)
+
+    # .replace("\\", "/") to make path string more consistent
+    return format_path(temp_dir, temp_name, last, temp_ext).replace("\\", "/")
+
+
+def format_path(temp_dir, temp_name, index, temp_ext):
+    """example_dir_path/file-0000%.ext"""
+
+    return f"{temp_dir}/{temp_name}-{str(index).zfill(5)}.{temp_ext}"
+
+
 def retrieve_width(argv_, cap_len_argv, DEFAULT_width):
     """take width from argv, dependably by numeber of argv"""
 
@@ -240,90 +324,6 @@ def retrieve_width(argv_, cap_len_argv, DEFAULT_width):
         return width
     else:
         return DEFAULT_width
-
-
-def parse_optional_argvs(argv_, cap_len_argv, DEFAULT_width):
-    """check if width, search_by_ratio are correct. If both are it returns width"""
-
-    check_correctness_optional_argvs(argv_, cap_len_argv)
-
-    return retrieve_width(argv_, cap_len_argv, DEFAULT_width)
-
-
-def main():
-    """Parsing sys.argv to pass them into right functions"""
-
-    check_argv_correctness(sys.argv)
-    program_help(sys.argv)
-
-    # Init variables
-    original_ref_path = sys.argv[1]
-    app_ref_path = sys.argv[2]
-    mode = sys.argv[3]
-
-    # [-1] To avoid checking 3 places at one, this argument is always last
-    by_ratio = bool(sys.argv[-1] in ARGV["search by ratio"])
-
-    similar_list = create_similar_images_list(
-        original_ref_path, app_ref_path, by_ratio)
-
-    width = 360  # Default
-
-    if mode in ARGV["save"]:
-
-        mode_save(width, similar_list, by_ratio)
-
-    elif mode in ARGV["show"]:
-
-        # Optional args
-        mode_show(width, similar_list, by_ratio)
-    else:
-        raise ValueError("Error: Invalid mode value\n"
-                         f" {mode}")
-
-    sys.exit(0)
-
-
-def mode_show(width, similar_list, by_ratio):
-    """show matched images"""
-
-    # Optional args
-    if len(sys.argv) >= 6:
-        width = parse_optional_argvs(sys.argv, 6, width)
-
-    # Process all images, show user each sequence one by one
-    for similar_pair in similar_list:
-
-        if not similar_pair is None:
-
-            images = compute_image_differences(similar_pair, by_ratio)
-
-            show_images(images, width)
-
-            print('NOTE: Press the "0" key, to close opened windows')
-            cv2.waitKey(0)
-
-
-def mode_save(width, similar_list, by_ratio):
-    """save matched images in chosen directory"""
-
-    if len(sys.argv) >= 5:
-        output_path = sys.argv[4]
-    else:
-        output_path = None
-
-    # Optional args
-    if len(sys.argv) >= 6:
-        width = parse_optional_argvs(sys.argv, 7, width)
-
-    # Process all images, save each sequence in chosen director
-    for similar_pair in similar_list:
-
-        if not similar_pair is None:
-
-            images = compute_image_differences(similar_pair, by_ratio)
-
-            save_images_as_one(images, output_path, width)
 
 
 if __name__ == "__main__":
