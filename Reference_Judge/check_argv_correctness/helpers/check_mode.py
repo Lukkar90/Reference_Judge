97a34d20e4ab679afb174128a5c16f7a4ae5bba5
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-08-17 14:56:24 +0200

    check mode

--- Reference_Judge/check_argv_correctness/helpers/check_mode.py
+++ Reference_Judge/check_argv_correctness/helpers/check_mode.py
@@ -1,3 +1,6 @@
+"""check correctnes of mode args and accompanying arguments"""
+
+
 # Python libs
 import sys
 
@@ -31,6 +34,16 @@ def check_mode(argv_):
 def check_mode_save(argv_):
     """check correctness all argv in save mode"""
 
+    # USE ONLY HERE
+    def is_output_path(argv_):
+        return len(argv_) < 5
+
+    def is_last_argv_by_ratio(argv_):
+        return argv_[6] in ARGV["search by ratio"]
+
+    def is_5th_legit_argv(argv_):
+        return is_legit_width(argv_, 5) or is_5th_by_ratio(argv_)
+
     if is_output_path(argv_):
         sys.exit("Error: No output path\n"
                  f"{help_tip()}")
@@ -41,43 +54,35 @@ def check_mode_save(argv_):
                  f" {argv_[5]}\n"
                  f"{help_tip()}")
 
-    elif len(argv_) == 7 and not is_last_argv_by_ratio(argv_):
+    elif len(argv_) == 7:
 
-        if not argv_[5].isnumeric():
-            print('Error: 5th should be numeric.\n')
+        if not is_legit_width(argv_, 5):
+            sys.exit('Error: 5th should be numeric.\n')
 
-        if argv_[6] not in ARGV["search by ratio"]:
+        if not is_last_argv_by_ratio(argv_):
             sys.exit(f'Error: 6th, last argument should be {ARGV["search by ratio"][0]}:\n'
                      f" {argv_[6]}\n"
                      f"{help_tip()}")
 
 
-def is_output_path(argv_):
-    return len(argv_) < 5
-
-
-def is_last_argv_by_ratio(argv_):
-    return argv_[6] in ARGV["search by ratio"]
-
-
-def is_5th_legit_argv(argv_):
-    return (argv_[5].isnumeric() or argv_[5] in ARGV["search by ratio"])
-
-
 def check_mode_show(argv_):
     """check correctness all argv in show mode"""
 
-    if len(argv_) == 5 and not (argv_[4].isnumeric() or argv_[4] in ARGV["search by ratio"]):
+    # USE ONLY HERE
+    def is_4th_legit_argv(argv_):
+        return is_legit_width(argv_, 4) or argv_[4] in ARGV["search by ratio"]
+
+    if len(argv_) == 5 and not is_4th_legit_argv(argv_):
         sys.exit(f'Error: 4th, last argument should be numeric or be {ARGV["search by ratio"][0]}:\n'
                  f" {argv_[4]}\n"
                  f"{help_tip()}")
 
     elif len(argv_) == 6:
 
-        if not argv_[4].isnumeric():
-            print('Error: 4th should be numeric.\n')
+        if not is_legit_width(argv_, 4):
+            sys.exit('Error: 4th should be numeric.\n')
 
-        if argv_[5] not in ARGV["search by ratio"]:
+        if not is_5th_by_ratio(argv_):
             sys.exit(f'Error: 5th, last argument should be {ARGV["search by ratio"][0]}:\n'
                      f" {argv_[5]}\n"
                      f"{help_tip()}")
@@ -86,3 +91,13 @@ def check_mode_show(argv_):
         sys.exit("Error: one argument too much:\n"
                  f" {argv_[6]}\n"
                  f"{help_tip()}")
+
+
+def is_5th_by_ratio(argv_):
+    """return bool"""
+    return argv_[5] in ARGV["search by ratio"]
+
+
+def is_legit_width(argv_, i):
+    """return bool"""
+    return argv_[i].isnumeric()
