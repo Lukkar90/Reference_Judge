30a8b131bf348e153d4ce0567dd5348aa3381112
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-09-10 18:22:27 +0200

    description added

--- __main__.py
+++ __main__.py
@@ -1,3 +1,35 @@
+"""
+NAME
+
+    Reference-Judge
+
+DESCRIPTION
+
+    Showing visual differences between images
+    ========================================
+
+    Reference-Judge is used for developers to show visual differences
+    between the app's particular screen and reference created by the app's designer.
+
+    It aims to improve workflow for the programmer and also designer.
+
+    For a programmer this tool available instant check of the screen,
+    if it is done according to references.
+
+    For the designer, this tool relieve him/her from the task of constant checking,
+    if a particular screen was done according to the reference.
+
+    Of course, it can be used for any other matching images purposes
+
+    This program uses image recognition algorithms from https://opencv.org/
+
+AUTHOR
+
+    Karol Łukaszczyk
+    e-mail: lukkarcontact@gmail.com
+"""
+
+
 import sys
 from manage import execute_from_command_line
 
